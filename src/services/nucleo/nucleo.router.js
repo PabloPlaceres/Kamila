47693e9b7ae072559617dcc72c56710a62d0b011
Pablo <pabloplacere@gmail.com>
2024-05-30 17:17:44 -0400

    18

--- src/services/nucleo/nucleo.router.js
+++ src/services/nucleo/nucleo.router.js
@@ -23,10 +23,10 @@ verifi], crearNucleo)
 .put("/nucleo/:id", [verifiToken, verificarTokenAdministrador, check('nombre', 'Debe incluir un nombre').not().isEmpty(),
 check('cantMilitante', 'Debe ser un numero decimal ').isDecimal(),
 check('cantMilitante', 'Debe incluir cantidad de militante ').not().isEmpty(),
-check('fondoSidical', 'Debe ser un numero float ').isFloat(),
+check('fondoSindical', 'Debe ser un numero float ').isFloat(),
 check('presupuestoAnual', 'Debe ser un numero float ').isFloat(),
 check('presupuestoMensual', 'Debe ser un numero float ').isFloat(),
-check('fondoSidical', 'Debe incluir un fondo sidical').not().isEmpty(),
+check('fondoSindical', 'Debe incluir un fondo sidical').not().isEmpty(),
 check('presupuestoAnual', 'Debe incluir un presupuesto anual').not().isEmpty(),
 check('presupuestoMensual', 'Debe incluir un presupuesto mensual').not().isEmpty(),
 verifi], actualizarNucleo)
