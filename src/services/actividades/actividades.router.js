277ba577f5d065ee0f082f3da502b40bbeef9199
Pablo <pabloplacere@gmail.com>
2024-06-01 19:41:31 -0400

    vauyulklkl

--- src/services/actividades/actividades.router.js
+++ src/services/actividades/actividades.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import verifi from "../../middleware/verifiExpres.js";
-import { actualizarActividad, crearActividad, eliminarActividad, listarActividades } from "./controllers/actividades.controllers.js";
+import { actualizarActividad, crearActividad, eliminarActividad, filtro, listarActividades } from "./controllers/actividades.controllers.js";
 import verifiToken from "../../middleware/verifiToken.js";
 import verificarTokenAdmiPlan from "../../middleware/verificarTokenAdmiPlan.js";
 import verificarTokenAdmiPres from "../../middleware/verificarTokenAdmiPres.js";
@@ -25,5 +25,6 @@ check('lugar', 'Debe incluir un lugar').not().isEmpty(),
 check('hora', 'Debe incluir una hora').not().isEmpty(),
 check('implicado', 'Debe incluir un implicado').not().isEmpty(),
 check('costo', 'Debe incluir un costo').not().isEmpty(), verifi], actualizarActividad)
+.get("/actividad/filtro", filtro)
 
 export default actividadRouter
