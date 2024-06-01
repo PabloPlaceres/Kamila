289ea5b113568b9ca88c5873938396bcf8be1365
Pablo <pabloplacere@gmail.com>
2024-06-01 19:08:45 -0400

    vauyuyu

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import router from "./routes/router.js"
 import fileUpload from "express-fileupload";
+import  multer  from 'multer';
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, file.fieldname + '-' + uniqueSuffix);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 
 const testRouter = express.Router();
--- src/services/usuario/usuario.router.js
+++ src/services/usuario/usuario.router.js
@@ -12,7 +12,7 @@ import  multer  from 'multer';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads/');
+        cb(null, '../../upload');
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
