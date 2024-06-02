f23b948e2f278e6bdc7d6782c1e54a6d3afd95c7
Pablo <pabloplacere@gmail.com>
2024-06-02 17:53:09 -0400

    ppppioiofff

--- src/services/usuario/usuario.router.js
+++ src/services/usuario/usuario.router.js
@@ -14,21 +14,17 @@ import multer from "multer";
 import { v4 as uuidv4 } from 'uuid';
 import path from "path";
 
-const verifyPortada = () => {
-    return multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, 'upload');
-        },
-        filename: (req, file, cb) => {
-            cb(null, `${uuidv4()}${path.extname(file.originalname)}`);
-        },
-    });
-};
-
-
-
-const storage = verifyPortada();
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    }
+}); 
 const upload = multer({ storage });
+
+
 const usuarioRouter = Router()
 
 usuarioRouter 
